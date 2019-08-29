4ecf3abc628485675ed62c4038bd46f14490c215
Samantha Berkholz <samanthaberkholz18@Samanthas-MacBook-Pro.local>
2019-08-29 13:51:45 -0500

    my first assignment commit

---
+++ sketch.js
@@ -0,0 +1,45 @@
+function setup() {
+    createCanvas(800, 600);
+    background(255, 255, 255);
+    fill(0);
+    //stroke(255);
+    
+    ellipse(350, 350, 800, 800);
+    
+    strokeWeight(50);
+    stroke(255);
+    ellipse(350, 350, 600, 600);
+    
+    
+    ellipse(350, 350, 200, 600);
+    
+    
+    ellipse(350, 350, 700, 200);
+    
+    
+    ellipse(350, 350, 180, 180);
+   
+    strokeWeight(10);
+    stroke(0);
+    ellipse(350, 350, 80, 80);
+
+    strokeWeight(80);
+    stroke(0);
+    ellipse(350, 350, 60, 60);
+
+
+    strokeWeight(30);
+    stroke(255);
+    ellipse(350, 350, 40, 40);
+
+    ellipse(350, 300, 45, 45);
+
+    
+
+    ellipse(350, 400, 45, 45);
+
+}
+
+function draw() {
+
+}
